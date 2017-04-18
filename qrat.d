4dd6b5608374cb55fb66cf678f3da0db60208a5f
H. S. Teoh <hsteoh@quickfur.ath.cx>
2017-04-18 12:17:56 -0700

    Implement binary operators with scalars.
    Implement +, - between QRats.

--- qrat.d
+++ qrat.d
@@ -101,19 +101,109 @@ struct QRat(int r, Num = long)
     /**
      * Binary operators.
      */
-    QRat opBinary(string op)(QRat qr)
+    QRat opBinary(string op)(QRat q)
         if (op == "+" || op == "-")
     {
-        if (c == qr.c)
-            return QRat(mixin("a "~op~" qr.a"), mixin("b "~op~" qr.b"), c);
+        if (c == q.c)
+            return QRat(mixin("a "~op~" q.a"), mixin("b "~op~" q.b"), c);
 
-        auto g = gcd(c, qr.c);
-        auto k1 = qr.c / g;
+        auto g = gcd(c, q.c);
+        auto k1 = q.c / g;
         auto k2 = c / g;
         auto leastMult = c*k1;
-        return QRat(mixin("a*k1 "~op~" qr.a*k1"), mixin("b*k1 "~op~" qr.b*k2"),
+        return QRat(mixin("a*k1 "~op~" q.a*k2"), mixin("b*k1 "~op~" q.b*k2"),
                     leastMult);
     }
+
+    static if (r==5 && is(Num == long))
+    unittest
+    {
+        auto q1 = QRat!5(1, 1, 2);
+        auto q2 = QRat!5(1, 1, 3);
+
+        assert(q1 + q1 == QRat!5(1, 1, 1));
+        assert(q1 + q2 == QRat!5(5, 5, 6));
+    }
+
+    /// ditto
+    QRat opBinary(string op, N)(N n)
+        if ((op == "+" || op == "-") &&
+            is(typeof(Num.init * N.init) : Num))
+    {
+        return QRat(mixin("a "~op~" n*c"), b, c);
+    }
+
+    static if (r==5 && is(Num == long))
+    unittest
+    {
+        auto q = QRat!5(1, 1, 2) + 1;
+        assert(q == QRat!5(3, 1, 2));
+        assert(q - 1 == QRat!5(1, 1, 2));
+    }
+
+    /// ditto
+    QRat opBinaryRight(string op, N)(N n)
+        if ((op == "+" || op == "-") &&
+            is(typeof(Num.init * N.init) : Num))
+    {
+        return QRat(mixin("n*c "~op~" a"), b, c);
+    }
+
+    static if (r==5 && is(Num == long))
+    unittest
+    {
+        auto q = 1 + surd!5;
+        assert(q == QRat!5(1, 1, 1));
+    }
+
+    // Scalar multiplication
+    /// ditto
+    QRat opBinary(string op, N)(N n)
+        if (op == "*" &&
+            is(typeof(Num.init * N.init) : Num))
+    {
+        return QRat(a*n, b*n, c);
+    }
+
+    static if (r==5 && is(Num == long))
+    unittest
+    {
+        auto q = 1 + surd!5;
+        assert(q*2 == QRat!5(2, 2, 1));
+    }
+
+    /// ditto
+    QRat opBinaryRight(string op, N)(N n)
+        if (op == "*" &&
+            is(typeof(Num.init * N.init) : Num))
+    {
+        return QRat(n*a, n*b, c);
+    }
+
+    static if (r==5 && is(Num == long))
+    unittest
+    {
+        auto q = 1 + 2*surd!5;
+        assert(q == QRat!5(1, 2, 1));
+    }
+
+    /// ditto
+    QRat opBinary(string op, N)(N n)
+        if (op == "/" &&
+            is(typeof(Num.init * N.init) : Num))
+    {
+        return QRat(a, b, c*n);
+    }
+
+    static if (r==5 && is(Num == long))
+    unittest
+    {
+        auto q = (1 + surd!5)/2;
+        assert(q == QRat!5(1, 1, 2));
+    }
+
+    // QRat multiplication
+    /// ditto
 }
 
 unittest
